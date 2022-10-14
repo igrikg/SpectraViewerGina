5393de7d61f5c2042b99a21b2c811c01f28205a2
Igor Gapon Work <igrikg@gmail.com>
2022-10-14 16:36:14 +0200

    Tree add

--- Test.1.py
+++ Test.1.py
@@ -1,51 +1,75 @@
-from matplotlib.widgets import EllipseSelector, RectangleSelector
-import numpy as np
-import matplotlib.pyplot as plt
+import sys
+from collections import deque
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import datetime
+from PyQt5.QtCore import *
 
 
-def select_callback(eclick, erelease):
-    """
-    Callback for line selection.
+class StandardItem(QStandardItem):
+    def __init__(self, txt='', image_path=''):
+        super().__init__()
+        self.setEditable(False)
+        self.setText(txt)
 
-    *eclick* and *erelease* are the press and release events.
-    """
-    x1, y1 = eclick.xdata, eclick.ydata
-    x2, y2 = erelease.xdata, erelease.ydata
-    print(f"({x1:3.2f}, {y1:3.2f}) --> ({x2:3.2f}, {y2:3.2f})")
-    print(f"The buttons you used were: {eclick.button} {erelease.button}")
+        if image_path:
+            image = QImage(image_path)
+            self.setData(image, Qt.DecorationRole)
+class view(QWidget):
+    def __init__(self, data):
+        super(view, self).__init__()
+        self.tree = QTreeView(self)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.tree)
+        self.model = QStandardItemModel()
+        self.model.setHorizontalHeaderLabels(['Name', 'Height', 'Weight'])
+        self.tree.header().setDefaultSectionSize(180)
+        self.tree.setModel(self.model)
+        self.importData(data)
+        self.tree.expandAll()
 
-
-def toggle_selector(event):
-    print('Key pressed.')
-    if event.key == 't':
-        for selector in selectors:
-            name = type(selector).__name__
-            if selector.active:
-                print(f'{name} deactivated.')
-                selector.set_active(False)
+    def importData(self, data, root=None):
+        self.model.setRowCount(0)
+        if root is None:
+            root = self.model.invisibleRootItem()
+        seen = {}   # List of  QStandardItem
+        values = deque(data)
+        while values:
+            value = values.popleft()
+            if value['unique_id'] == 1:
+                parent = root
             else:
-                print(f'{name} activated.')
-                selector.set_active(True)
+                pid = value['parent_id']
+                if pid not in seen:
+                    values.append(value)
+                    continue
+                parent = seen[pid]
+            unique_id = value['unique_id']
 
 
-fig = plt.figure(constrained_layout=True)
-axs = fig.subplots(2)
 
-N = 100000  # If N is large one can see improvement by using blitting.
-x = np.linspace(0, 10, N)
-
-selectors = []
-for ax, selector_class in zip(axs, [RectangleSelector, EllipseSelector]):
-    ax.plot(x, np.sin(2*np.pi*x))  # plot something
-    ax.set_title(f"Click and drag to draw a {selector_class.__name__}.")
-    selectors.append(selector_class(
-        ax, select_callback,
-        useblit=True,
-        button=[1, 3],  # disable middle button
-        minspanx=5, minspany=5,
-        spancoords='pixels',
-        interactive=True))
-    fig.canvas.mpl_connect('key_press_event', toggle_selector)
-axs[0].set_title("Press 't' to toggle the selectors on and off.\n"
-                 + axs[0].get_title())
-plt.show()
+            parent.appendRow([
+                QStandardItem(value['short_name']),
+                QStandardItem(value['height']),
+                QStandardItem(value['weight'])
+            ])
+            seen[unique_id] = parent.child(parent.rowCount() - 1)
+if __name__ == '__main__':
+    data = [
+        {'unique_id': 1, 'parent_id': 0, 'short_name': '', 'height': ' ', 'weight': ' '},
+        {'unique_id': 2, 'parent_id': 1, 'short_name': 'Class 1', 'height': ' ', 'weight': ' '},
+        {'unique_id': 3, 'parent_id': 2, 'short_name': 'Lucy', 'height': '162', 'weight': '50'},
+        {'unique_id': 4, 'parent_id': 2, 'short_name': 'Joe', 'height': '175', 'weight': '65'},
+        {'unique_id': 5, 'parent_id': 1, 'short_name': 'Class 2', 'height': ' ', 'weight': ' '},
+        {'unique_id': 6, 'parent_id': 5, 'short_name': 'Lily', 'height': '170', 'weight': '55'},
+        {'unique_id': 7, 'parent_id': 5, 'short_name': 'Tom', 'height': '180', 'weight': '75'},
+        {'unique_id': 8, 'parent_id': 1, 'short_name': 'Class 3', 'height': ' ', 'weight': ' '},
+        {'unique_id': 9, 'parent_id': 8, 'short_name': 'Jack', 'height': '178', 'weight': '80'},
+        {'unique_id': 10, 'parent_id': 8, 'short_name': 'Tim', 'height': '172', 'weight': '60'}
+    ]
+    app = QApplication(sys.argv)
+    view = view(data)
+    view.setGeometry(300, 100, 600, 300)
+    view.setWindowTitle('QTreeview Example')
+    view.show()
+    sys.exit(app.exec_())
